1243c7bf660fac38dc09c4e3dffcee7f3f10afa0
Natac13 <Natac13@user.noreply.github.com>
2015-08-09 19:24:13 -0400

    Fixed the rounding error with help from the gitter channel

--- exact_change.js
+++ exact_change.js
@@ -29,11 +29,11 @@ function drawer(price, cash, cid) {
       if(change_due / string_to_number[unit[0]] >= 1) {
         var count = 0.00;
         while(change_due / string_to_number[unit[0]] >= 1 && initial_amount > 0) {
+          change_due = +(change_due).toFixed(2);
           change_due -= string_to_number[unit[0]];
           initial_amount -= string_to_number[unit[0]];
           count++;
         }
-        if(unit[0] === 'PENNY') { count++;}
         change_arr.push([unit[0], (string_to_number[unit[0]]*count)]);
         return [unit[0], initial_amount];
       } else {
@@ -47,5 +47,5 @@ function drawer(price, cash, cid) {
 }
 
 
-
-drawer(19.50, 20.00, [['PENNY', 1.01], ['NICKEL', 2.05], ['DIME', 3.10], ['QUARTER', 4.25], ['ONE', 90.00], ['FIVE', 55.00], ['TEN', 20.00], ['TWENTY', 60.00], ['ONE HUNDRED', 100.00]]);
+var x = drawer(19.50, 20.00, [['PENNY', 1.01], ['NICKEL', 2.05], ['DIME', 3.10], ['QUARTER', 4.25], ['ONE', 90.00], ['FIVE', 55.00], ['TEN', 20.00], ['TWENTY', 60.00], ['ONE HUNDRED', 100.00]]);
+console.log(x);
